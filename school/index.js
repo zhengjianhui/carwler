cc23aaf540b5f4d48e05c7a947a802d7bf144e3d
usr/local/bin <815168733@qq.com>
2016-06-19 22:20:49 +0800

    Revert "carwler"
    This reverts commit 45b8c1c90b73fa57fae7185bef9c92acd6c3155e.

--- school/index.js
+++
@@ -1,5 +0,0 @@
-// 引入class.js模块
-var klass = require('./klass');
-// 调用模块的暴露的add方法
-
-klass.cs('德山',['狗狗','叶狗']);
--- school/teacher.js
+++
@@ -1,8 +0,0 @@
-function add(teacher) {
-  console.log('add teacher:' + teacher);
-}
-
-
-// 将模块暴露 exports 可以声明模块
-// exports 可以挂载合法的js 对象
-exports.add = add
